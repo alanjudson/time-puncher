4e9d98f1f6db63d08060d044993ea39c44b7b9b2
alanjudson <alanjudson+code.com>
2019-04-18 10:26:43 -0400

    Adds users with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_18_034643) do
+ActiveRecord::Schema.define(version: 2019_04_18_035031) do
 
   create_table "punchtimes", force: :cascade do |t|
     t.string "punch_type"
     t.string "description"
     t.datetime "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
