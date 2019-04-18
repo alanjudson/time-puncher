b274680df59ebf80fff2f0ad388eb9eaef23c5d6
alanjudson <alanjudson+code.com>
2019-04-18 13:16:24 -0400

    Adds session controller, views, routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+  def create
+    # authenticate - match user namem/password
+    if user = User.is_authentic(params[:email], params[:password])
+      # get a session hash with user id
+      session[:user_id] = user.id
+      flash[:notice] = "Hi there, #{user.name}!"
+      redirect_to user
+    else
+      flash.now[:alert] = "Wrong password, try again."
+      render :new
+      # redirect
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "signup" => "users#new"
+
+  resources :sessions
   resources :users
+
   root "punchtimes#index"
   resources :punchtimes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
