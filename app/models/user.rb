5fa797ea9145b4d1a9e422b336c59472dff772b8
alanjudson <alanjudson+code.com>
2019-04-19 13:21:33 -0400

    Destroys punchtimes when user is destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :punchtimes
+  has_many :punchtimes, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true,
