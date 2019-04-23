8efcb69d556eabcdb0db04144ea8bff9ba55d46c
alanjudson <alanjudson+code.com>
2019-04-23 14:57:21 -0400

    Adds admin role to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_143210) do
+ActiveRecord::Schema.define(version: 2019_04_23_154849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_04_19_143210) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "punchtimes", "users"
